6910873daabaf5f9509af492738d2bd1d65eb56e
sophieperryy-3 <sophie.perry@uk.ey.com>
2025-12-04 19:20:27 +0000

    Use LabRole for Lambda deployment (AWS Academy compatible)

--- infrastructure-simple/lambda.tf
+++ infrastructure-simple/lambda.tf
@@ -5,22 +5,35 @@ data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
 
+# S3 bucket for Lambda code
+resource "aws_s3_bucket" "lambda_code" {
+  bucket = "todo-lambda-code-${data.aws_caller_identity.current.account_id}"
+}
+
+# Upload Lambda zip to S3
+resource "aws_s3_object" "lambda_zip" {
+  bucket = aws_s3_bucket.lambda_code.id
+  key    = "lambda-deployment.zip"
+  source = "${path.module}/lambda-deployment.zip"
+  etag   = filemd5("${path.module}/lambda-deployment.zip")
+}
+
 # Lambda function
 resource "aws_lambda_function" "api" {
-  filename         = "${path.module}/lambda-deployment.zip"
+  s3_bucket        = aws_s3_bucket.lambda_code.id
+  s3_key           = aws_s3_object.lambda_zip.key
   function_name    = "todo-api"
-  role            = data.aws_iam_role.lab_role.arn
-  handler         = "lambda.handler"
+  role             = data.aws_iam_role.lab_role.arn
+  handler          = "lambda.handler"
   source_code_hash = filebase64sha256("${path.module}/lambda-deployment.zip")
-  runtime         = "nodejs20.x"
-  timeout         = 30
-  memory_size     = 256
+  runtime          = "nodejs20.x"
+  timeout          = 30
+  memory_size      = 256
 
   environment {
     variables = {
       DYNAMODB_TABLE_NAME = aws_dynamodb_table.tasks.name
-      AWS_REGION         = var.aws_region
-      NODE_ENV           = "production"
+      NODE_ENV            = "production"
     }
   }
 }
