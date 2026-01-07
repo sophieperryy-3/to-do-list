dea0b86405914ff1119778269f177874f4bffb8a
sophieperryy-3 <sophie.perry@uk.ey.com>
2026-01-07 19:58:02 +0000

    test: intentionally break Terraform formatting
    This should:
    1. Trigger Terraform CI (which will FAIL)
    2. Block CD pipeline from running
    3. Prove security gate works

--- infrastructure-simple/variables.tf
+++ infrastructure-simple/variables.tf
@@ -14,7 +14,7 @@ variable "dynamodb_table_name" {
 }
 
 variable "frontend_bucket_name" {
-  description = "S3 bucket name for frontend static assets"
-  type        = string
-  default     = "todo-frontend-simple"
+description = "S3 bucket name for frontend static assets"
+type        = string
+default     = "todo-frontend-simple"
 }
