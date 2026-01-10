0c78c90dd6f9abbf7e53af1cbbe4092fc138f79d
sophieperryy-3 <sophie.perry@uk.ey.com>
2026-01-10 14:53:07 +0000

    fix: terraform formatting for backend-setup.tf

--- infrastructure-simple/backend-setup.tf
+++ infrastructure-simple/backend-setup.tf
@@ -44,9 +44,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
